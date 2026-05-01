427be097c08d10bc42907573bb9e14779fcea012
Spyros Seimenis <sespiros@gmail.com>
2026-05-02 02:05:55 +0300

    release: v0.3.2

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
       # on the host Xcode.app toolchain. Build with `nix build --impure`.
       notchify = pkgs.stdenvNoCC.mkDerivation {
         pname = "notchify";
-        version = "0.3.1";
+        version = "0.3.2";
         src = ./.;
 
         # Allow access to /Applications/Xcode.app and /usr/bin/codesign
